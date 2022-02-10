bea7092cc260a69c437cefac0493a7027f44b222
Arne Peirs <arnepeirs@hotmail.com>
2022-02-10 11:50:59 +0100

    Replaced `node-fetch` with `heroku-client`

--- src/commands/admin/herstart.js
+++ src/commands/admin/herstart.js
@@ -1,54 +1,32 @@
-const { default: fetch } = require('node-fetch');
+const Heroku = require('heroku-client');
 const { ICommand } = require('../../typings');
-const utils = require('../../utils/functions');
 
 /** @type {ICommand} */
 module.exports = {
   name: 'herstart',
   aliases: ['restart'],
   description: 'Herstart de bot.',
-  cooldown: 60,
+  cooldown: 300,
   permissions: ['ADMINISTRATOR'],
   ownerOnly: true,
   slash: 'both',
   info: {
-    maxArgs: 1,
-    expectedArgs: '[tijd]',
-    examples: ['herstart 20s', 'herstart 5m'],
+    maxArgs: 0,
+    examples: ['herstart'],
   },
   async execute(message, args, client) {
-    // Get the provided time in seconds. Defaults to 1 minute.
-    const limit = utils.parseTimeLimit(args[0]) || 60;
+    // Logs channel
+    const channel = client.channels.cache.get('934494331627253760');
 
-    // Check if parseTimeLimit returned an error message
-    if (typeof limit === 'string') return ['reply', limit];
+    // Check if the channel is found and if it's a text channel
+    if (!channel?.isText()) return;
 
-    const timeText = utils.formatToTime(limit);
+    await channel.send('Opnieuw opstarten...');
 
-    // Message all the possible guilds about restarting the bot
-    let channel;
-    for (const info of client.guildInfo) {
-      const guild = client.guilds.cache.get(info[0]);
-      channel = guild.channels.cache.get(info[1].examChannel);
+    // Restart the dyno's on Heroku
+    const heroku = new Heroku({ token: process.env.HEROKU_OAUTH });
+    heroku.delete('/apps/mnm-examen-bot/dynos');
 
-      // Check if the guild has a channel set, otherwise message the guild owner
-      if (!channel?.isText()) channel = await guild.fetchOwner();
-
-      await channel.send(`Attentie! Over ${timeText} zal ik mezelf opnieuw opstarten.`);
-    }
-
-    // Send a request to Heroku to restart the bot
-    return setTimeout(async () => {
-      channel.send('Opnieuw opstarten...');
-
-      await fetch('https://api.heroku.com/apps/mnm-exam-bot/dynos', {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/vnd.heroku+json; version=3',
-          'Authorization': `Bearer ${process.env.HEROKU_OAUTH}`,
-        },
-      });
-    }, limit * 1000);
+    return ['reply', 'Commando uitgevoerd.'];
   },
 };
